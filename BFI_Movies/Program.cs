099ef8fc0a06d33c7781066e5eb18806c8a95b9b
Lewis Newson <lewisnewson@gmail.com>
2018-10-07 14:16:33 +0100

    VS initial files

---
+++ BFI_Movies/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BFI_Movies
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
